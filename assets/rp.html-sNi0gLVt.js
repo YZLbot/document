615c64f770611052398a3b0b09e15cef2817393d
Takeoff0518 <Takeoff0518@users.noreply.github.com>
2023-12-24 11:10:38 +0000

    Deploying to gh-pages from @ YZLbot/document@664ee2736bed80e13bebddd2eb9b7fdeaade4c50 🚀

---
+++ assets/rp.html-sNi0gLVt.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as n,e as t}from"./app-n-2bpVWH.js";const c={},r=t(`<div class="hint-container info"><p class="hint-container-title">相关信息</p><p>来源：诸葛神算。</p></div><h2 id="抽签" tabindex="-1"><a class="header-anchor" href="#抽签" aria-hidden="true">#</a> 抽签</h2><p>进行抽签。</p><div class="language-消息" data-ext="消息"><pre class="language-消息"><code>[第 349 签]
+
+风波今已息，舟楫遇安流，自此功名遂，何须叹白头。
+</code></pre></div><h2 id="解签" tabindex="-1"><a class="header-anchor" href="#解签" aria-hidden="true">#</a> 解签</h2><p>解释抽到的签是什么内容。</p><div class="language-消息" data-ext="消息"><pre class="language-消息"><code>[第 349 签:解签]
+
+一切纷争已息，今后行程应平稳无险，刻下经营多顺利，虽慢了些又何妨？
+</code></pre></div>`,7),s=[r];function d(i,o){return a(),n("div",null,s)}const p=e(c,[["render",d],["__file","rp.html.vue"]]);export{p as default};
