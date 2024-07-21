0e1df41c095989b29ccb945859848a37212a4ec1
Takeoff0518 <Takeoff0518@users.noreply.github.com>
2024-07-21 03:23:20 +0000

    Deploying to gh-pages from @ YZLbot/document@d51788972ac4e4bb205a3c931aa6f7c1dd2fa7b0 🚀

---
+++ assets/index.html-sLhw5Xa5.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{o,c as s,a as e}from"./app-B-UMMowC.js";const n="/assets/header-mTxaEN4J.png",c={},_=e("p",null,"Kira~",-1),r=e("div",{align:"center"},[e("img",{width:"100%",src:n,alt:"logo"})],-1),a=[_,r];function i(l,d){return o(),s("div",null,a)}const h=t(c,[["render",i],["__file","index.html.vue"]]);export{h as default};
